4b8f2fa5bc42214d1466cedc02d18af53517fb49
Deshna Shivhare <deshnashivhare1806@gmail.com>
2026-04-27 13:57:02 +0530

    Update predictor.py

--- utils/predictor.py
+++ utils/predictor.py
@@ -1,1 +1,14 @@
 
+import joblib
+
+model = joblib.load("models/job_model.pkl")
+
+def predict_job(skill_match,test_score,projects,experience):
+
+    result = model.predict_proba(
+        [[skill_match,test_score,projects,experience]]
+    )
+
+    probability = result[0][1] * 100
+
+    return round(probability,2)
