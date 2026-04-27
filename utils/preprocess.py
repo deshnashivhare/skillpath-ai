da97c1470a3d8d405df264ee8ac86c957d0552de
Deshna Shivhare <deshnashivhare1806@gmail.com>
2026-04-27 13:57:35 +0530

    Implement text preprocessing with NLTK
    Added text preprocessing function using NLTK.

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,1 +1,15 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
+def preprocess_text(text):
+
+    tokens = word_tokenize(text.lower())
+
+    filtered = []
+
+    for word in tokens:
+        if word.isalpha() and word not in stopwords.words('english'):
+            filtered.append(word)
+
+    return filtered
