6e7108d499cbc9573c19b0c18e32bc914cff400e
Deshna Shivhare <deshnashivhare1806@gmail.com>
2026-04-27 13:56:29 +0530

    Add job roles and gap analysis function

--- utils/gap_analysis.py
+++ utils/gap_analysis.py
@@ -1,1 +1,21 @@
+job_roles = {
 
+"Data Analyst": ["python","sql","excel","data analysis"],
+
+"ML Engineer": ["python","machine learning","tensorflow"],
+
+"Web Developer": ["html","css","javascript"]
+
+}
+
+def analyze_gap(user_skills, role):
+
+    required = job_roles[role]
+
+    matched = set(user_skills) & set(required)
+
+    missing = set(required) - set(user_skills)
+
+    match_percent = (len(matched)/len(required))*100
+
+    return round(match_percent,2), list(missing)
