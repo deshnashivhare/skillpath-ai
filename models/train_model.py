e3066f35467dab1aba371ff0d9530440b02962b7
Deshna Shivhare <deshnashivhare1806@gmail.com>
2026-04-30 10:29:04 +0530

    Implement logistic regression model training
    Added code to train a logistic regression model using data from a CSV file and save the model to a file.

--- models/train_model.py
+++ models/train_model.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib
 
+data = pd.read_csv("dataset/dataset.csv")
+
+X = data[['skill_match','test_score','projects','experience']]
+y = data['selected']
+
+model = LogisticRegression()
+
+model.fit(X,y)
+
+joblib.dump(model,"models/job_model.pkl")
+
+print("Model trained successfully")
